69282354b2d34e9cd46eaf461714aa33895c731f
Thịnh521 <nguyenphucthinh2005tp@gmail.com>
2025-04-10 16:00:04 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import CustomerRoutes from "./routes/CustomerRoutes";
-import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
     <div className="bg-white text-black dark:bg-neutral-900 dark:text-white min-h-screen duration-200 overflow-hidden">
-      <AnimatePresence mode="wait">
-        <CustomerRoutes />
-      </AnimatePresence>
+      <CustomerRoutes />
     </div>
   );
 }
