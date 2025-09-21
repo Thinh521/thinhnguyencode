bd18da09e4ad296a85e18d75dadae19711cbc493
Thịnh521 <nguyenphucthinh2005tp@gmail.com>
2025-09-21 11:00:38 +0700

    Hoàn thành: Giao diện và logic Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import CustomerRoutes from "./routes/CustomerRoutes";
+import Preloader from "./components/Preloader/Preloader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Preloader loading={loading} text="Phúc Thịnh" />;
+  }
+
   return (
     <div className="bg-white text-black dark:bg-neutral-900 dark:text-white min-h-screen duration-200 overflow-hidden">
       <CustomerRoutes />
---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import "./Preloader.css";
+
+const Preloader = ({ loading = true, text = "Grilli" }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!loading) {
+      const timeout = setTimeout(() => setIsLoaded(true), 300);
+      return () => clearTimeout(timeout);
+    } else {
+      setIsLoaded(false);
+    }
+  }, [loading]);
+
+  return (
+    <div
+      className={`preload bg-white text-black dark:bg-neutral-900 dark:text-white ${
+        isLoaded ? "loaded" : ""
+      }`}
+    >
+      <div className="w-12 h-12 relative transform rotate-45 -top-12">
+        <div
+          className="absolute bg-neutral-900 dark:bg-white w-5 h-5 animate-ping"
+          style={{ top: 0, left: 0, animationDuration: "1.2s" }}
+        />
+        <div
+          className="absolute bg-neutral-900 dark:bg-white w-5 h-5 animate-ping"
+          style={{
+            top: 0,
+            right: 0,
+            animationDuration: "1.2s",
+            animationDelay: "0.15s",
+          }}
+        />
+        <div
+          className="absolute bg-neutral-900 dark:bg-white w-5 h-5 animate-ping"
+          style={{
+            bottom: 0,
+            right: 0,
+            animationDuration: "1.2s",
+            animationDelay: "0.3s",
+          }}
+        />
+        <div
+          className="absolute bg-neutral-900 dark:bg-white w-5 h-5 animate-ping"
+          style={{
+            bottom: 0,
+            left: 0,
+            animationDuration: "1.2s",
+            animationDelay: "0.45s",
+          }}
+        />
+      </div>
+      <p className="text">{text}</p>
+    </div>
+  );
+};
+
+export default Preloader;
