1638952426c1d533bebc607d88dbddbe5792a61e
Thịnh521 <nguyenphucthinh2005tp@gmail.com>
2025-10-01 15:35:19 +0700

    Thêm: thư viện

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,8 +1,6 @@
-// src/lib/utils.js
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Gộp class tailwind một cách an toàn
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
