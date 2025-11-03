974608d3ad57b2e1d4e28f79293fc492bd6df30a
Thịnh521 <nguyenphucthinh2005tp@gmail.com>
2025-11-03 08:23:34 +0700

    Update: data

--- src/data/StoryData.jsx
+++ src/data/StoryData.jsx
@@ -14,7 +14,17 @@ const StorytData = [
   {
     id: 2,
     title: "Câu chuyện cuộc sống",
-    imgae: IMAGES.story1_1,
+    imgae: IMAGES.story1_2,
+    title_2: "Viết về cuộc sống",
+    date: "07-04-2025",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus veniam laboriosam ipsa autem. Laudantium, vel delectus excepturi facere quos commodi distinctio architecto? Numquam iure suscipit quisquam est hic voluptatibus nihil.",
+    author: "@p.thjnhngn",
+  },
+  {
+    id: 2,
+    title: "Câu chuyện bản thân",
+    imgae: IMAGES.story1_3,
     title_2: "Viết về cuộc sống",
     date: "07-04-2025",
     description:
