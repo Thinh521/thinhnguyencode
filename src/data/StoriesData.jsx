6e480de18363123c07702995b0145fd98fdb4a95
Thịnh521 <nguyenphucthinh2005tp@gmail.com>
2026-03-16 16:21:12 +0800

    Update

--- src/data/StoriesData.jsx
+++ src/data/StoriesData.jsx
@@ -9,6 +9,16 @@ const storiesData = [
     username: "_thjnh.pn_",
     video: "story_2.mp4",
   },
+  {
+    id: 3,
+    username: "_thjnh.pn_",
+    video: "story_3.mp4",
+  },
+  {
+    id: 4,
+    username: "_thjnh.pn_",
+    video: "story_4.mp4",
+  },
 ];
 
 export default storiesData;
