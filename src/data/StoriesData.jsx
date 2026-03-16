ca4bdd194589185b5cb84e6a609e87c454f74b7f
Thịnh521 <nguyenphucthinh2005tp@gmail.com>
2026-03-16 16:11:17 +0800

    Update

--- src/data/StoriesData.jsx
+++ src/data/StoriesData.jsx
@@ -9,36 +9,6 @@ const storiesData = [
     username: "_thjnh.pn_",
     video: "story_2.mp4",
   },
-  {
-    id: 3,
-    username: "_thjnh.pn_",
-    video: "story_3.mp4",
-  },
-  {
-    id: 5,
-    username: "_thjnh.pn_",
-    video: "story_5.mp4",
-  },
-  {
-    id: 6,
-    username: "_thjnh.pn_",
-    video: "story_6.mp4",
-  },
-  {
-    id: 7,
-    username: "_thjnh.pn_",
-    video: "story_7.mp4",
-  },
-  {
-    id: 8,
-    username: "_thjnh.pn_",
-    video: "story_8.mp4",
-  },
-  {
-    id: 9,
-    username: "_thjnh.pn_",
-    video: "story_9.mp4",
-  },
 ];
 
 export default storiesData;
